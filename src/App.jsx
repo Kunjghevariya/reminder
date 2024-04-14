bdcc655fe0609ebe0dfbb1d5cd5d7b280b763d24
Kunj Ghevariya <kunj.ghevariya@gmail.com>
2024-04-14 17:18:47 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import Fg from './components/Fg'
 const App = () => {
-  const data = [{ name: "kunj ghevariya", reminde: "birthday", date: "04/04/25", desc: "hapyy birthday" }
-    , { name: "Fento", reminde: "birthday", date: "13 Jun 2024 ", desc: "hapyy birthday" }, { name: "lambo", reminde: "birthday", date: "28 Nov 2024 ", desc: "hapyy birthday" }
-    , { name: "ved", reminde: "birthday", date: "14 Dec 2024 ", desc: "hapyy birthday" }, { name: "darsh", reminde: "birthday", date: "19 Nov 2024 ", desc: "hapyy birthday" }
-    , { name: "harsh", reminde: "birthday", date: "1 sept 2024 ", desc: "hapyy birthday" },{ name : "mitho" , reminde : "birthday" , date : "26 jan 2025 " , desc : "hapyy birthday"}
-    ,{ name : "keval" , reminde : "birthday" , date : "12 Jan 2025 " , desc : "hapyy birthday"},{ name : "vedant" , reminde : "birthday" , date : "17 Nov 2024 " , desc : "hapyy birthday"}
-,{ name : "divyesh" , reminde : "birthday" , date : "08 Nov 2024 " , desc : "hapyy birthday"},{ name : "nado" , reminde : "birthday" , date : "28 april 2024 " , desc : "hapyy birthday"}
-,{ name : "bhavin" , reminde : "birthday" , date : "03 dec 2024 " , desc : "hapyy birthday"},{ name : "meet" , reminde : "birthday" , date : "22 dec 2024 " , desc : "hapyy birthday"}
-,{ name : "mino" , reminde : "birthday" , date : "12 dec 2024" , desc : "hapyy birthday"}]
+  const data = [{ name: "Kunj Ghevariya", reminde: "birthday", date: "04/04/25", desc: "hapyy birthday" }
+    , { name: "Fento", reminde: "birthday", date: "13 Jun 2024 ", desc: "hapyy birthday" }, { name: "Lambo", reminde: "birthday", date: "28 Nov 2024 ", desc: "hapyy birthday" }
+    , { name: "Lathiyo", reminde: "birthday", date: "14 Dec 2024 ", desc: "hapyy birthday" }, { name: "Matko", reminde: "birthday", date: "19 Sept 2024 ", desc: "hapyy birthday" }
+    , { name: "Pekko 🤖", reminde: "birthday", date: "1 sept 2024 ", desc: "hapyy birthday" },{ name : "Tikadi 💊" , reminde : "birthday" , date : "26 jan 2025 " , desc : "hapyy birthday"}
+    ,{ name : "Kondo" , reminde : "birthday" , date : "12 Jan 2025 " , desc : "hapyy birthday"},{ name : "Vedu" , reminde : "birthday" , date : "17 Nov 2024 " , desc : "hapyy birthday"}
+,{ name : "Divlo 👩‍⚕️" , reminde : "birthday" , date : "08 Sept 2024 " , desc : "hapyy birthday"},{ name : "Nado" , reminde : "birthday" , date : "28 april 2024 " , desc : "hapyy birthday"}
+,{ name : "Bhavlo" , reminde : "birthday" , date : "03 dec 2024 " , desc : "hapyy birthday"},{ name : "Buttho" , reminde : "birthday" , date : "22 dec 2024 " , desc : "hapyy birthday"}
+,{ name : "Mino 👈 👉" , reminde : "birthday" , date : "30 Sept 2024" , desc : "hapyy birthday"}]
   return (
     <>
     <div className="tittle text-center text-6xl">Reminder</div>
