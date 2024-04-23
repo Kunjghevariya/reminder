21a351fe9ec6879f68af364de11c91dc05dab211
Kunj Ghevariya <kunj.ghevariya@gmail.com>
2024-04-23 14:33:36 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,17 @@ const App = () => {
   const data = [
     { name: "Kunj Ghevariya", reminde: "birthday", date: "04/04/25", desc: "happy birthday" },
     { name: "Fento 🍹", reminde: "birthday", date: "13 Jun 2024", desc: "happy birthday" },
-    { name: "Lambo", reminde: "birthday", date: "28 Nov 2024", desc: "happy birthday" },
-    { name: "Lathiyo", reminde: "birthday", date: "14 Dec 2024", desc: "happy birthday" },
+    { name: "Lambo 🤓🦒", reminde: "birthday", date: "28 Nov 2024", desc: "happy birthday" },
+    { name: "Lathiyo 🐆", reminde: "birthday", date: "14 Dec 2024", desc: "happy birthday" },
     { name: "Matko 🛢️", reminde: "birthday", date: "19 Sept 2024", desc: "happy birthday" },
     { name: "Pekko 🤖", reminde: "birthday", date: "1 Sept 2024", desc: "happy birthday" },
     { name: "Tikadi 💊", reminde: "birthday", date: "26 Jan 2025", desc: "happy birthday" },
-    { name: "Kondo 🔫", reminde: "birthday", date: "12 Jan 2025", desc: "happy birthday" },
-    { name: "Vedu", reminde: "birthday", date: "17 Nov 2024", desc: "happy birthday" },
+    { name: "Kondo 🔫🥸", reminde: "birthday", date: "12 Jan 2025", desc: "happy birthday" },
+    { name: "Vedu 😎", reminde: "birthday", date: "17 Nov 2024", desc: "happy birthday" },
     { name: "Divlo 👩‍⚕️", reminde: "birthday", date: "08 Sept 2024", desc: "happy birthday" },
-    { name: "Nado", reminde: "birthday", date: "28 April 2024", desc: "happy birthday" },
-    { name: "Bhavlo", reminde: "birthday", date: "03 Dec 2024", desc: "happy birthday" },
-    { name: "Buttho", reminde: "birthday", date: "22 Dec 2024", desc: "happy birthday" },
+    { name: "Nado 😤", reminde: "birthday", date: "28 April 2024", desc: "happy birthday" },
+    { name: "Bhavlo 🙄", reminde: "birthday", date: "03 Dec 2024", desc: "happy birthday" },
+    { name: "Buttho 💁‍♂️", reminde: "birthday", date: "22 Dec 2024", desc: "happy birthday" },
     { name: "Mino 👈 👉", reminde: "birthday", date: "30 Sept 2024", desc: "happy birthday" }
   ];
 
